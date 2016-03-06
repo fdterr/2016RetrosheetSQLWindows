4805f156b4399f69a69127f72fb6a32b540c7334
Techgraphs <techgraphs@gmail.com>
2016-03-06 17:12:22 -0600

    Initial File Upload

---
+++ retrosheet(x64)/data/zipped/get_zip_files.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\gnuwin32\bin\wget.exe" -i ./retrosheet_zip_files.txt
+"C:\Program Files\7-Zip\7z.exe" e *.zip -y -oC:\Retrosheet\data\unzipped
---
+++ retrosheet(x86)/data/zipped/get_zip_files.bat
@@ -0,0 +1,2 @@
+C:\progra~1\gnuwin32\bin\wget.exe -i ./retrosheet_zip_files.txt
+"C:\Program Files\7-Zip\7z.exe" e *.zip -y -oC:\Retrosheet\data\unzipped
